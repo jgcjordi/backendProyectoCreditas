6eefba5147b520119705ec4fa367edbf3ac34367
Jordi Gomis <jgc.jordi@gmail.com>
2020-05-05 19:29:57 +0200

    trying deploy heroku added transactional to @lob

--- src/main/kotlin/com/creditas/backendphones/product/domain/entities/Model.kt
+++ src/main/kotlin/com/creditas/backendphones/product/domain/entities/Model.kt
@@ -2,9 +2,11 @@ package com.creditas.backendphones.product.domain.entities
 
 import com.fasterxml.jackson.annotation.JsonBackReference
 import javax.persistence.*
+import javax.transaction.Transactional
 
 @Entity
 @Table
+@Transactional
 data class Model(
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
